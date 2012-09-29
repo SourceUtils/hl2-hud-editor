217c28de38158768b958c562cc6aa223945f8b3a
TimePath <andrew.hardaker1995@gmail.com>
2012-09-30 01:39:13 +1000

    Cleanup.

--- TF2 WYSIWYG HUD EDITOR/src/com/timepath/tf2/EditorFrame.java
+++ TF2 WYSIWYG HUD EDITOR/src/com/timepath/tf2/EditorFrame.java
@@ -2,8 +2,11 @@ package com.timepath.tf2;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.DisplayMode;
 import java.awt.FileDialog;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -38,78 +41,45 @@ import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeSelectionModel;
 import net.tomahawk.XFileDialog;
 
-
-
 /**
+ * Links of interest:
+ *
+ * UI principles:
+ * http://developer.apple.com/legacy/mac/library/#technotes/tn/tn2042.html
+ * http://developer.apple.com/library/mac/#technotes/tn2002/tn2110.html#//apple_ref/doc/uid/DTS10003202
+ *
+ *
+ * http://java.dzone.com/news/native-dialogs-swing-little
+ * http://code.google.com/p/xfiledialog/
+ * http://today.java.net/pub/a/today/2004/01/29/swing.html
+ *
+ * http://www.javaprogrammingforums.com/java-swing-tutorials/7944-how-use-jtree-create-file-system-viewer-tree.html
+ *
+ * http://www.horstmann.com/articles/Taming_the_GridBagLayout.html
+ *
+ * Reference editors:
+ * http://visualhud.pk69.com/
+ * http://gamebanana.com/css/tools/4483
+ * http://img13.imageshack.us/img13/210/hudmanagerss.png
+ * http://plrf.org/superhudeditor/screens/0.3.0/superhudeditor-0.3.0-linux.jpg
  *
  * @author andrew
  */
-// ctrl + click and drag is weird
-// http://gamebanana.com/css/tools/4483
-// http://img13.imageshack.us/img13/210/hudmanagerss.png
-// http://plrf.org/superhudeditor/screens/0.3.0/superhudeditor-0.3.0-linux.jpg
-// http://visualhud.pk69.com/
-// http://www.javaprogrammingforums.com/java-swing-tutorials/7944-how-use-jtree-create-file-system-viewer-tree.html
-// http://www.horstmann.com/articles/Taming_the_GridBagLayout.html
-// http://developer.apple.com/legacy/mac/library/#technotes/tn/tn2042.html
-// http://today.java.net/pub/a/today/2004/01/29/swing.html
-
-// Credits:
-// http://code.google.com/p/xfiledialog/
-
+@SuppressWarnings("serial")
 public class EditorFrame extends JFrame implements ActionListener {
-    
-    private final static OS os;
-    private final static int shortcutKey;
-    
-    private enum OS {
-        Windows, Mac, Other
-    }
-    static {
-        String vers = System.getProperty("os.name").toLowerCase();
-        if(vers.indexOf("windows") != -1) {
-            os = OS.Windows;
-        } else if(vers.indexOf("mac") != -1) {
-            os = OS.Mac;
-        } else {
-            os = OS.Other;
-        }
-        
-        if(os == OS.Windows) {
-            shortcutKey = ActionEvent.CTRL_MASK;
-            XFileDialog.setTraceLevel(0);
-        } else if(os == OS.Mac) {
-            shortcutKey = ActionEvent.META_MASK;
-            System.setProperty("com.apple.macos.useScreenMenuBar", "true");
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name application property", "TF2 HUD Editor");
-        } else {
-            shortcutKey = ActionEvent.CTRL_MASK;
-        }
-        
-//        final JOptionPane optionPane = new JOptionPane(os, JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION);
-    }
-    
-    private static void systemLAF() {
+
+    public static void main(String... args) {
+        boolean overrideLAF = false;
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    private static void programLAF() {
-        try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
+            if(overrideLAF) {
+                for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
                 }
+            } else {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             }
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
@@ -120,10 +90,7 @@ public class EditorFrame extends JFrame implements ActionListener {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             Logger.getLogger(EditorFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    
-    public static void main(String... args) {
-        programLAF();
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -132,87 +99,139 @@ public class EditorFrame extends JFrame implements ActionListener {
             }
         });
     }
-    
-    private HudCanvas canvas;
-    private ResLoader resloader;
-    
-    private JTree fileTree;
-    
-    private DefaultMutableTreeNode hudFilesRoot;
-    private MyJTable propTable;
-    
+
+    //<editor-fold defaultstate="collapsed" desc="OS specific code">
+    private final static OS os;
+    private final static int shortcutKey;
+
+    private enum OS {
+        Windows, Mac, Linux, Other
+    }
+
+    static {
+        String osVer = System.getProperty("os.name").toLowerCase();
+        if(osVer.indexOf("windows") != -1) {
+            os = OS.Windows;
+        } else if(osVer.indexOf("mac") != -1 || osVer.indexOf("OS X") != -1) {
+            os = OS.Mac;
+        } else if(osVer.indexOf("linux") != -1) {
+            os = OS.Linux;
+        } else {
+            os = OS.Other;
+            System.out.println("Unrecognised OS: " + osVer);
+        }
+
+        if(os == OS.Windows) {
+            shortcutKey = ActionEvent.CTRL_MASK;
+            XFileDialog.setTraceLevel(0);
+        } else if(os == OS.Mac) {
+            shortcutKey = ActionEvent.META_MASK;
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.macos.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name application property", "TF2 HUD Editor");
+        } else if(os == OS.Linux) {
+            shortcutKey = ActionEvent.CTRL_MASK;
+        } else {
+            shortcutKey = ActionEvent.CTRL_MASK;
+        }
+    }
+    //</editor-fold>
+
     public EditorFrame() {
+        DisplayMode d = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
+//        Dimension screenRes = Toolkit.getDefaultToolkit().getScreenSize();
+//        long gcm = gcm(screenRes.width, screenRes.height);
+//        System.out.println((screenRes.width / gcm) + ":" + (screenRes.height / gcm));
+
         this.setTitle("TimePath's WYSIWYG TF2 HUD Editor");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(600, 400));
-        this.setPreferredSize(new Dimension(600, 400));
+        this.setPreferredSize(new Dimension((int)(d.getWidth() / 1.5), (int)(d.getHeight() / 1.5)));
         this.setLocationRelativeTo(null);
-        
-        createMenu(this);
-        
-//        JPanel browser = new JPanel(new GridBagLayout());
+
+        createMenu();
+
         JSplitPane browser = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-        
+
         createTree(browser);
         createProperties(browser);
-        browser.setResizeWeight(0.5);
         
+        browser.setResizeWeight(0.5);
+
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, createCanvas(), browser);
 //        splitPane.setDividerLocation(f.getPreferredSize().width-350);
         splitPane.setResizeWeight(0.8);
         this.add(splitPane);
-        
+
         this.pack();
     }
 
-    public void start() {        
+    /**
+     * Finds the greatest common multiple
+     * @param a
+     * @param b
+     * @return
+     */
+    public static long gcm(long a, long b) {
+        return b == 0 ? a : gcm(b, a % b);
+    }
+
+    private HudCanvas canvas;
+    private ResLoader resloader;
+
+    private JTree fileTree;
+
+    private DefaultMutableTreeNode hudFilesRoot;
+    private MyJTable propTable;
+
+    public void start() {
         this.setVisible(true);
         this.createBufferStrategy(2);
     }
-    
-    private void createMenu(JFrame f) {
+
+    private void createMenu() {
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         fileMenu.setMnemonic(KeyEvent.VK_F);
         fileMenu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
         menuBar.add(fileMenu);
-        
+
         JMenuItem openItem = new JMenuItem("Open...", KeyEvent.VK_O);
         openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, shortcutKey));
         openItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
         openItem.addActionListener(this);
         fileMenu.add(openItem);
-        
+
         fileMenu.addSeparator();
-        
+
         JMenuItem exitItem = new JMenuItem("Exit", KeyEvent.VK_X);
         exitItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
         exitItem.addActionListener(this);
         fileMenu.add(exitItem);
-        
-        
-        
+
+
+
         JMenu editMenu = new JMenu("Edit");
         editMenu.setMnemonic(KeyEvent.VK_E);
         editMenu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
         menuBar.add(editMenu);
-        
+
         JMenuItem selectAllItem = new JMenuItem("Select All", KeyEvent.VK_A);
         selectAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, shortcutKey));
         selectAllItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
         selectAllItem.addActionListener(this);
         editMenu.add(selectAllItem);
-        
+
         JMenuItem resolutionItem = new JMenuItem("Change Resolution", KeyEvent.VK_R);
         resolutionItem.setEnabled(false);
         resolutionItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, shortcutKey));
         resolutionItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
         resolutionItem.addActionListener(this);
         editMenu.add(resolutionItem);
-        
-        f.setJMenuBar(menuBar);
+
+        this.setJMenuBar(menuBar);
     }
-    
+
     private JScrollPane createCanvas() {
         canvas = new HudCanvas();
         canvas.setPreferredSize(new Dimension(800, 600));
@@ -223,51 +242,51 @@ public class EditorFrame extends JFrame implements ActionListener {
         p.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         return p;
     }
-    
+
     private void createTree(Container p) {
         hudFilesRoot = new DefaultMutableTreeNode("HUD");
-        
+
         fileTree = new JTree(hudFilesRoot);
         fileTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-        
+
         fileTree.addTreeSelectionListener(new TreeSelectionListener() {
             @Override
             public void valueChanged(TreeSelectionEvent e) {
                 DefaultTableModel model = (DefaultTableModel) propTable.getModel();
                 model.getDataVector().removeAllElements();
                 propTable.scrollRectToVisible(new Rectangle(0, 0, 0, 0));
-                
+
                 DefaultMutableTreeNode node = (DefaultMutableTreeNode) fileTree.getLastSelectedPathComponent();
                 if (node == null) {
                     return;
                 }
-                
+
                 Object nodeInfo = node.getUserObject();
                 if(nodeInfo instanceof Element) {
                     Element element = (Element) nodeInfo;
-                    
+
                     for(Map.Entry<KVPair<String,String>,String> entry : element.getProps().entrySet()) {
                         model.insertRow(model.getRowCount(), new Object[] {entry.getKey().getKey(), entry.getKey().getValue(), entry.getValue()});
                     }
-                    
+
                     canvas.load(element);
-                    
+
 //                    ColumnsAutoSizer.sizeColumnsToFit(propTable);
-//                    
+//
 //                    for(int i = 0; i < propTable.getColumnCount(); i++) {
 //                        propTable.getColumnModel().getColumn(i).setMinWidth(propTable.getColumnModel().getColumn(i).getMaxWidth());
 //                    }
-//        
+//
 //                    propTable.getColumnModel().getColumn(propTable.getColumnCount() - 1).setMinWidth(1000);
 //                    propTable.getColumnModel().getColumn(propTable.getColumnCount() - 1).setMaxWidth(1000);
-                    
+
                 }
             }
         });
-        
+
         JScrollPane treeView = new JScrollPane(fileTree);
         treeView.setPreferredSize(new Dimension(400, 400));
-        
+
 //        GridBagConstraints c = new GridBagConstraints();
 //        c.gridx = 2;
 //        c.gridy = 0;
@@ -276,13 +295,13 @@ public class EditorFrame extends JFrame implements ActionListener {
 //        c.fill = GridBagConstraints.BOTH;
 //        c.weightx = 0.25;
 //        c.weighty = 1;
-//        
+//
         p.add(treeView);//, c);
     }
-    
+
     private void createProperties(Container p) {
         DefaultMutableTreeNode top = new DefaultMutableTreeNode("Properties");
-        
+
         MyJTableModel model = new MyJTableModel();
         model.addColumn("Key");
         model.addColumn("Value");
@@ -295,7 +314,7 @@ public class EditorFrame extends JFrame implements ActionListener {
 
         JScrollPane treeView = new JScrollPane(propTable);
         treeView.setPreferredSize(new Dimension(400, 400));
-        
+
 //        GridBagConstraints c = new GridBagConstraints();
 //        c.gridx = 2;
 //        c.gridy = 1;
@@ -307,53 +326,41 @@ public class EditorFrame extends JFrame implements ActionListener {
         p.add(treeView);//, c);
     }
 
+    /**
+     * Start in the home directory
+     * linux = ~
+     * windows = %userhome% (I think)
+     * mac = ?
+     */
     private void locateHudDirectory() {
         String selection = null;
-        
-    
-//        XFileDialog dlg=new XFileDialog(helloworld.this); // save file  
-
-//    dlg.setTitle(title7);
-//
-//    // Important: we do not implement the setMode() method, 
-//    // instead, we use the caller function "getSaveFile()" 
-//    // to tell XFileDialog to use "Save File Mode" 
-//    // 
-//    //
-//    String content =dlg.getSaveFile(); 
-//    String folder = dlg.getDirectory();         
-//    selectioncontent.setText("filename:" + content); 
-//    
-//    System.out.println("APP>>: current Dir: " + folder ); 
-//
-//    dlg.dispose(); 
         if(os == OS.Windows) {
-            XFileDialog dlg = new XFileDialog(EditorFrame.this);    
+            XFileDialog dlg = new XFileDialog(EditorFrame.this);
             dlg.setTitle("Open HUD");
-//            ArrayList<String> ext0= new ArrayList<String>(); ext0.add("*"); 
-//            ExtensionsFilter filter = new ExtensionsFilter("All Files", ext0);
-//            dlg.addFilters(filter); 
             selection = dlg.getFolder();
             dlg.dispose();
-        } else if(os == OS.Mac) {
+        } else if(os == OS.Mac) { // Pretty close on linux too, though files are selectable and not just directories.
             System.setProperty("apple.awt.fileDialogForDirectories", "true");
             System.setProperty("com.apple.macos.use-file-dialog-packages", "true");
-            FileDialog fd = new FileDialog(this, "File Dialog");
-            java.awt.FileDialog d = new java.awt.FileDialog();
-//            d.setm
-//            fd.setMultipleMode(false);
+            FileDialog fd = new FileDialog(this, "Open HUD");
+            fd.setMultipleMode(false); // specific to java 7 - the default on anything lower
             fd.setVisible(true);
             selection = fd.getFile();
             System.setProperty("apple.awt.fileDialogForDirectories", "false");
             System.setProperty("com.apple.macos.use-file-dialog-packages", "false");
-        } else {
-            final JFileChooser fc = new JFileChooser();
+        } else if(os == OS.Linux) {
+            FileDialog fd = new FileDialog(this, "Open HUD");
+            fd.setMultipleMode(false); // specific to java 7 - the default on anything lower
+            fd.setVisible(true);
+            selection = fd.getFile();
+        } else { // Fallback to swing
+            JFileChooser fc = new JFileChooser();
             fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             if(fc.showOpenDialog(EditorFrame.this) == JFileChooser.APPROVE_OPTION) {
                 selection = fc.getSelectedFile().getPath();
             }
         }
-        
+
         if(selection != null) {
             File file = new File(selection);
 
@@ -420,7 +427,7 @@ public class EditorFrame extends JFrame implements ActionListener {
         public void propertyChange(PropertyChangeEvent e) {
         String prop = e.getPropertyName();
 
-        if (dialog.isVisible() 
+        if (dialog.isVisible()
         && (e.getSource() == optionPane)
         && (prop.equals(JOptionPane.VALUE_PROPERTY))) {
         //If you were going to check something
@@ -444,5 +451,5 @@ public class EditorFrame extends JFrame implements ActionListener {
 
         canvas.setPreferredSize(new Dimension(1920, 1080));
     }
-    
+
 }
