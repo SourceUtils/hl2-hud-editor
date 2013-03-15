c87098ca4629e5de5a19efc642522eb4c0ee0989
TimePath <andrew.hardaker1995@gmail.com>
2013-03-15 18:14:01 +1100

    Refactored, suppressed Ayatana error message

--- src/com/timepath/tf2/hudeditor/EditorFrame.java
+++ src/com/timepath/tf2/hudeditor/EditorFrame.java
@@ -1,4 +1,4 @@
-package com.timepath.tf2.hudeditor.gui;
+package com.timepath.tf2.hudeditor;
 
 import apple.OSXAdapter;
 import com.boxysystems.jgoogleanalytics.FocusPoint;
@@ -654,7 +654,7 @@ public final class EditorFrame extends javax.swing.JFrame {
             }
         } else if(OS.isLinux()) {
             if(!Ayatana.installMenu((JFrame) this, menubar)) {
-                error("AyatanaDesktop failed to load" + "\nDE:" + System.getenv("XDG_CURRENT_DESKTOP"));
+//                error("AyatanaDesktop failed to load" + "\nDE:" + System.getenv("XDG_CURRENT_DESKTOP"));
             }
         }
     }
@@ -1768,10 +1768,6 @@ public final class EditorFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JSplitPane rootSplit;
     private javax.swing.JSplitPane sideSplit;
     private com.timepath.swing.StatusBar status;
